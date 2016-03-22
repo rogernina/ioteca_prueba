1a110f0f068981057a73f77718fcf2edcd47a62f
Edwin Calsin Quinto <edwincqu@gmail.com>
2016-03-22 16:25:48 -0500

    builds --

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,23 @@ gulp.task('serve', function() {
         host: 'localhost'
     });
 });
+
+
+
+	function compileJs(app, model){
+		console.log('');
+		console.log('Proyect ERP-UPeU');
+		console.log('build javascript | '+app+' : '+model+' |');
+		return gulp.src(['src/apps/'+app+'/'+model+'/**/*.js'])
+		.pipe(concat(''+model+'.min.js'))
+		.pipe(uglify({ mangle: false }))
+		.pipe(gulp.dest('build/apps/'+app+'/'+model+''));
+	}
+
+	function compileHtml(app, model){
+		console.log('');
+		console.log('build DOM html | '+app+' : '+model+' |');
+		return gulp.src(['src/apps/'+app+'/'+model+'/views/*.html'])
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('build/apps/'+app+'/'+model+'/'))
+	}
